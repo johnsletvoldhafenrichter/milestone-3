270877d6f7f9a7560e84453fbb654bebff129d88
johnsletvoldhafenrichter <john.hafenrichter@yahoo.com>
2020-08-21 00:24:59 +0200

    fixed an error with my count DB, it updated the count and therefore did not put ids correctly

--- app/deleting.py
+++ app/deleting.py
@@ -2,14 +2,11 @@ from flask import render_template, session, request, redirect, url_for
 from bson.objectid import ObjectId
 
 from app import app
-from app.setup import DB_GAME_LIST, DB_REVIEWS, DB_USERS, DB_COUNTER, DB_GAME_SUGGESTION
+from app.setup import DB_GAME_LIST, DB_REVIEWS, DB_USERS, DB_GAME_SUGGESTION
 
 @app.route('/delete_game/<game_id>/<game_name>')
 def delete_game(game_id, game_name):
     if session['admin']:
-        count=int(DB_REVIEWS.find({'game_name': game_name}).count())
-        DB_COUNTER.update({'counter_name': 'counter'}, { '$inc': {'number_reviews': int(-count)}})
-        DB_COUNTER.update({'counter_name': 'counter'}, { '$inc': {'number_games': -1}})
         DB_REVIEWS.remove({'game_name': game_name})
         DB_GAME_LIST.remove({'_id': ObjectId(game_id)})
         return redirect(url_for('admin_tab_games'))
@@ -18,9 +15,6 @@ def delete_game(game_id, game_name):
 @app.route('/delete_user/<user_id>/<review_name>')
 def delete_user(user_id, review_name):
     if session['admin']:
-        count=int(DB_REVIEWS.find({'username': review_name}).count())
-        DB_COUNTER.update({'counter_name': 'counter'}, { '$inc': {'number_reviews': int(-count)}})
-        DB_COUNTER.update({'counter_name': 'counter'}, { '$inc': {'number_users': -1}})
         DB_REVIEWS.remove({'username': review_name})
         DB_USERS.remove({'_id': ObjectId(user_id)})
         return redirect(url_for('admin_tab_users'))
@@ -30,7 +24,6 @@ def delete_user(user_id, review_name):
 def delete_review(review_id):
     if session['username']:
         DB_REVIEWS.remove({'_id': ObjectId(review_id)})
-        DB_COUNTER.update({'counter_name': 'counter'}, { '$inc': {'number_reviews': -1}})
         if session['admin']:
             return redirect(url_for('admin_tab_reviews'))
         return redirect(url_for('your_reviews'))
@@ -50,7 +43,6 @@ def clear_sessions(where):
 @app.route('/delete_suggested_game/<game_id>/<game_name>')
 def delete_suggested_game(game_id, game_name):
     if session['admin']:
-        DB_COUNTER.update({'counter_name': 'counter'}, { '$inc': {'number_suggestions': -1}})
         DB_GAME_SUGGESTION.remove({'_id': ObjectId(game_id)})
         return redirect(url_for('admin_tab_suggestions'))
     return render_template('no_login.html')
